895ee7a5e5649773f6e94e736be09036a9dbf922
Fabien Fleutot <fleutot@gmail.com>
2012-11-22 21:37:05 +0100

    minor utilities around main

--- src/main.lua
+++ src/main.lua
@@ -1,9 +1,16 @@
 local sched = require 'sched'
 local telnet = require 'shell.telnet'
 
+function watch(em,ev)
+	sched.sighook (em, ev or '*', function(ev, ...)
+		printf("Signal: %s.%s(%s)", tostring(em), ev, sprint{...} :gsub (2, -2))
+	end)
+end 
+
 local function main ()
+	log.displaylogger = print
+	log.setlevel('INFO')
 	telnet.init{ editmode='edit', address='*', port=2323, historysize=256 }
-	print("Application started, connect a telnet to port 2323")
 end
 
 sched.run(main)
